8f209b29d6dbf57bed086ae0b4892434a75e983b
wshnng <wshnng@b92cab1e-0213-11df-804c-772d9fe11005>
2010-01-31 22:39:32 +0000

    Added back vehicle.cpp

---
+++ branches/Flo/CraterRacer/CraterRacer/Source Code/Entity Files/Vehicle.cpp
@@ -0,0 +1,10 @@
+/* 
+ * The generic vehicle entity.  AI and Player vehicles extend this class. 
+ */ 
+  
+ #include "Vehicle.h" 
+  
+ Vehicle::Vehicle( Device* device ): Entity( device ) 
+ { 
+ } 
+ 
