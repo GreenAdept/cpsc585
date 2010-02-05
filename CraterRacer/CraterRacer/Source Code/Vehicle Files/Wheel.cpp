8cc5ccff4f4bfc7702e9d92ce4d04e8c5cfc8b30
jmulmer2 <jmulmer2@gmail.com>
2010-02-05 06:17:05 +0000

    A few more updates for the wheels

---
+++ CraterRacer/CraterRacer/Source Code/Vehicle Files/Wheel.cpp
@@ -0,0 +1,27 @@
+#include "Wheel.h"
+
+//--------------------------------------------------------------------------------------
+// Function:  Constructor
+// Initializes the wheel object
+//--------------------------------------------------------------------------------------
+Wheel::Wheel() 
+{
+	m_rRestitution		= NxReal(0.5);
+	m_rStaticFriction	= NxReal(1.0);
+	m_rDynamicFriction	= NxReal(0.3);
+
+	m_fAngle			= 0; //degrees
+	m_fDiameter			= 0.5; //meters
+
+	m_mMaterial->setDynamicFriction(m_rDynamicFriction);
+	m_mMaterial->setStaticFriction(m_rStaticFriction);
+	m_mMaterial->setRestitution(m_rRestitution);
+}
+
+//--------------------------------------------------------------------------------------
+// Function:  Destructor
+//--------------------------------------------------------------------------------------
+Wheel::~Wheel() 
+{
+	//delete m_mMaterial; Doesn't work - someone know why?
+}
