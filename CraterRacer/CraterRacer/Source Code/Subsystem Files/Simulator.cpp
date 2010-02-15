cdc10e073721b0342daf47cd9fc6b8a5c68f540f
jmulmer2 <jmulmer2@gmail.com>
2010-02-15 22:57:49 +0000

    Changed vehicle mass back to 10kg

--- CraterRacer/CraterRacer/Source Code/Subsystem Files/Simulator.cpp
+++ CraterRacer/CraterRacer/Source Code/Subsystem Files/Simulator.cpp
@@ -18,7 +18,7 @@ Simulator::Simulator()
 	m_rStaticFriction		= NxReal(0.4);
 	m_rDynamicFriction		= NxReal(0.4);
 	m_rMaxAngularVelocity	= NxReal(2);
-	m_rVehicleMass			= 200.0;
+	m_rVehicleMass			= 10.0;
 	m_rWheelRestLength		= 0.3;
 	m_rMinWheelDisplacement = 1.25;
 	m_rSpringK				= ( -m_vDefaultGravity.y * m_rVehicleMass ) / ( m_rWheelRestLength * 4 );
