36302d54678c5e5d37230b86fbe270d99cd0edfa
tfalders <tfalders@b92cab1e-0213-11df-804c-772d9fe11005>
2010-02-22 21:01:00 +0000

    Created Waypoint class and attached it to the Terrain class. I plan to use it for AI pathfinding.

--- trunk/CraterRacer/CraterRacer/Source_Code/Entity_Files/Terrain.h
+++ trunk/CraterRacer/CraterRacer/Source_Code/Entity_Files/Terrain.h
@@ -7,26 +7,40 @@
 //--------------------------------------------------------
 
 #include "Entity.h"
+#include <vector>
+
 
 //---------------------------------------------------------
-//		CLASS: Terrain
-//
+// Class: Waypoint
+// Waypoint data structure to be used for AI pathing.
 //---------------------------------------------------------
 
-class Terrain: public Entity
-{
+struct Waypoint {
+	Vec3 position;
+	Waypoint* next;
 
+	Waypoint (Vec3 p) { position = p; }
+	~Waypoint () { delete next; }
+};
+
+
+//---------------------------------------------------------
+// Class: Terrain
+//---------------------------------------------------------
+
+class Terrain: public Entity {
 public:
-
 	//Public Interface ------------------------------------
+	Terrain () {}
+	~Terrain () { delete trackStart; }
 
-	Terrain( void ) { }
+	Waypoint* getTrackStart () { return trackStart; }
+	void      buildTrack (Vec3* waypoints, int size);
+	void      buildTrack (std::vector<Vec3> waypoints);
 	
-
 private:
-	
 	//Data Members ----------------------------------------
-
+	Waypoint* trackStart;
 };
 
 #endif //TERRAIN_H
