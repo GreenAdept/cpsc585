230d3336f37bb50d7cb40a12144b3afb8508dadd
tfalders <tfalders@b92cab1e-0213-11df-804c-772d9fe11005>
2010-02-24 01:29:43 +0000

    Made the Entity destructor virtual. Did some work on AI pathfinding.

--- CraterRacer/CraterRacer/Source_Code/Vehicle Files/VehicleAI.cpp
+++ CraterRacer/CraterRacer/Source_Code/Vehicle Files/VehicleAI.cpp
@@ -9,7 +9,17 @@
 // Function: think
 // A stub for the thinking section of the AI.
 //--------------------------------------------------------------------------------------
+
 void VehicleAI::think (EntityManager *em, int myList, int myIndex) {
+	if (state == AI::STOPPED) return;
+	if (destination == 0)
+		destination = em->getTerrain()->getTrackStart();
+
+	Vec3 myPos = em->getPosition (myList, myIndex);
+	Vec3 destPos = destination->getPosition ();
+	if (distSquared (myPos, destPos) < 25)
+		state = AI::TRIGGERED;
+
 	Input* input = em->getComputerInputObj (myIndex);
 	input->setInput (Input::UP, true);
 }
