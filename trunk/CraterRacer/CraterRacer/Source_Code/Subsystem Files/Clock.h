8679f976c2c8893b9d49150f147b6e1dd4a5c1e4
wshnng <wshnng@b92cab1e-0213-11df-804c-772d9fe11005>
2010-03-03 20:29:27 +0000

    Added Clock.h and Clock.cpp, haven't tested yet, and haven't changed any files to use this class yet.

---
+++ trunk/CraterRacer/CraterRacer/Source_Code/Subsystem Files/Clock.h
@@ -0,0 +1,27 @@
+#define  NOMINMAX
+#include "Windows.h"
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Clock{
+public:
+	void start(); //to start or reset the clock
+	bool isPaused(); //returns true if clock is currently paused
+	bool togglePause(); //change from paused to resume, and vice versa
+	int getTotalTimeInMS(); //returns the total time since calling start() or resume() in milliseconds
+	int getTotalTimeInSec(); //returns the total time since calling start() or resume() in milliseconds
+	int getElapsedTimeInMS(); //returns the elapsed time since last retrieved in milliseconds
+	int getElapsedTimeInSec(); //returns the elapsed time since last retrieved in seconds
+	string getFormattedTime(); //returns time as minutes:seconds:milliseconds
+
+private:
+	long startTimeMS;
+	int totalTimeMS;
+	int lastTimeMS; //last retrieved time
+	int totalPausedTimeMS;
+	int pausedTimeMS;
+	int startPausedMS;
+	bool paused;
+};
