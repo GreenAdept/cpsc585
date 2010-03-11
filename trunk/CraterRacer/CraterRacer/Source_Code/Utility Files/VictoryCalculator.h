c6aaa2b99520481c4c833157dcfd4e4d2cbeca56
jmulmer2@gmail.com <jmulmer2@gmail.com@b92cab1e-0213-11df-804c-772d9fe11005>
2010-03-11 00:04:58 +0000

    The game now records the 3 best times, and keeps track of them in the file times.txt.  The number of times recorded can be changed by a simple variable change.  If we don't want to, the times.txt file does not need to be included in the subversion.

--- trunk/CraterRacer/CraterRacer/Source_Code/Utility Files/VictoryCalculator.h
+++ trunk/CraterRacer/CraterRacer/Source_Code/Utility Files/VictoryCalculator.h
@@ -13,14 +13,17 @@
 class VictoryCalculator {
 	std::vector<bool> finished;
 	std::vector<int>  ranks;
+	bool			  recorded;
 
 	void    calculateRank      (std::vector<Vehicle*>& vehicles, int index);
 
 public:
+	VictoryCalculator();
 	void    calculateRanks     (std::vector<Vehicle*> vehicles);
 	int     getRank            (int index);
 	bool    isFinished         (int index);
 	wstring getFormattedString (int index);
+	void	recordTime		   (int milliseconds);
 };
 
 
