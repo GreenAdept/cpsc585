647dbd0fa8e8595c169609c7b0153a84ed8d745d
tfalders <tfalders@b92cab1e-0213-11df-804c-772d9fe11005>
2010-03-26 20:27:27 +0000

    Wrong way calculation seems to be working a bit better now. The big crater issue seems to be fixed.

--- CraterRacer/CraterRacer/Source_Code/Vehicle Files/AIPath.cpp
+++ CraterRacer/CraterRacer/Source_Code/Vehicle Files/AIPath.cpp
@@ -31,16 +31,6 @@ Vec3 Waypoint::getClosestPosition (Vec3 p) {
 	return positions[index];
 }
 
-Vec3 Waypoint::getDirectionToWP (Vec3 p) {
-	Vec3 temp = getClosestPosition(p) - p;
-	temp.y = 0.0f;
-	if (temp.x==0.0f && temp.z==0.0f)
-		return temp;
-
-	D3DXVec3Normalize (&temp, &temp);
-	return temp;
-}
-
 float Waypoint::getDistanceSquaredToWP (Vec3 p) {
 	float minDist = distSquared (positions[0], p);
 
@@ -57,6 +47,37 @@ float Waypoint::getDistanceToWP (Vec3 p) {
 }
 
 
+Vec3 Waypoint::getDirectionToWP_ClosestToGivenPos (Vec3 p) {
+	Vec3 temp = getClosestPosition(p) - p;
+	temp.y = 0.0f;
+	if (temp.x==0.0f && temp.z==0.0f)
+		return temp;
+
+	D3DXVec3Normalize (&temp, &temp);
+	return temp;
+}
+Vec3 Waypoint::getDirectionToWP_ClosestToGivenDir (Vec3 p, Vec3 d) {
+	Vec3 result;
+	int  resultCos = -1;
+
+	for (int i=0; i<positions.size(); i++) {
+		Vec3 temp = positions[i] - p;
+		temp.y = 0.0f;
+		if (temp.x==0.0f && temp.z==0.0f)
+			continue;
+
+		D3DXVec3Normalize (&temp, &temp);
+		float tempCos = D3DXVec3Dot (&d, &temp);
+		if (tempCos > resultCos) {
+			result = temp;
+			resultCos = tempCos;
+		}
+	}
+
+	return result;
+}
+
+
 
 
 Vec3 AIPath::getPositionOfWP (Vec3 p, int waypntIndex) {
@@ -64,7 +85,7 @@ Vec3 AIPath::getPositionOfWP (Vec3 p, int waypntIndex) {
 }
 
 Vec3 AIPath::getDirectionToWP (Vec3 p, int waypntIndex) {
-	return wps[waypntIndex % wps.size()].getDirectionToWP (p);
+	return wps[waypntIndex % wps.size()].getDirectionToWP_ClosestToGivenPos (p);
 }
 
 float AIPath::getDistanceSquaredToWP (Vec3 p, int waypntIndex) {
@@ -74,6 +95,11 @@ float AIPath::getDistanceSquaredToWP (Vec3 p, int waypntIndex) {
 bool AIPath::reachedWaypoint (Vec3 p, int waypntIndex, float waypntRadius) {
 	float d = wps[waypntIndex % wps.size()].getDistanceSquaredToWP (p);
 	return d <= waypntRadius * waypntRadius;
+}
+
+bool AIPath::goingWrongWay (Vec3 p, Vec3 d, int waypntIndex) {
+	Vec3 desiredDir = wps[(waypntIndex+1) % wps.size()].getDirectionToWP_ClosestToGivenDir (p, d);
+	return D3DXVec3Dot (&d, &desiredDir) < 0;
 }
 
 int AIPath::findCurrentLap (int passedWPs) {
